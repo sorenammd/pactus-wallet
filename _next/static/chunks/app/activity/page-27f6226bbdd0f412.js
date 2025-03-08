9a36621b14c23a7a1d94dc9af054c8aae9d38986
sorenammd <sorenammd@users.noreply.github.com>
2025-03-08 11:42:51 +0000

    deploy: 235f0d202a44d25f92e8f06bb12b1c5fa844f713

--- _next/static/chunks/app/activity/page-27f6226bbdd0f412.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[866],{1589:()=>{},9953:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,1589,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[496,587,84,358],()=>s(9953)),_N_E=e.O()}]);
