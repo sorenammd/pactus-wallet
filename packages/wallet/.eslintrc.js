f71f5f3a93cba6a276fb67150b1a7ccd1bc8d4fb
Henry Nguyen <henry@pactus.org>
2025-03-25 21:45:45 +0700

    ci(wallet): add CI workflows for wallet package linting and testing with gts

--- packages/wallet/.eslintrc.js
+++ packages/wallet/.eslintrc.js
@@ -1,3 +1,27 @@
 module.exports = {
-    extends: ['../../.eslintrc.js']
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    'no-case-declarations': 'warn',
+    '@typescript-eslint/no-namespace': 'warn',
+    'no-loss-of-precision': 'warn',
+  },
+  ignorePatterns: ['build/**/*', 'node_modules/**/*', '**/*.js', 'dist/**/*'],
 };
